512b1d0b27d67c2fab57c8012f713d9b21cbf586
lxw0110 <lxw1225363168@outlook.com>
2023-11-14 10:26:09 +0800

    add input pattern

--- CAD/main.cpp
+++ CAD/main.cpp
@@ -163,70 +163,141 @@ void ProcessMenu(int value)
 
 int main(int argc, char* argv[])
 {
-	double out_loop[][3]= { { 0.0, 0.0, 0.0 }, { 100.0, 0.0, 0.0 }, { 100.0, 100.0, 0.0 }, { 0.0, 100.0, 0.0 } };
-	double in_loop1[][3]= { { 20.0, 10.0, 0.0 }, { 40.0, 10.0, 0.0 }, { 40.0, 30.0, 0.0 }, { 20.0, 30.0, 0.0 } };
-	double in_loop2[][3]= { { 30.0, 70.0, 0.0 }, { 50.0, 70.0, 0.0 }, { 50.0, 90.0, 0.0 }, { 30.0, 90.0, 0.0 } };
-	double in_loop3[][3] = { { 20.0, 40.0, 0.0 }, { 40.0, 40.0, 0.0 }, { 40.0, 60.0, 0.0 }, { 20.0, 60.0, 0.0 } };
-	double in_loop4[][3] = { { 70.0, 45.0, 0.0 }, { 90.0, 45.0, 0.0 }, { 90.0, 65.0, 0.0 }, { 70.0, 65.0, 0.0 } };
-	double d[3] = { 0.0,0.0,60.0 };
-
-	vertex *outpoint[4];
-	vertex *inpoint1[4];
-	vertex *inpoint2[4];
-	vertex *inpoint3[4];
-	vertex *inpoint4[4];
-	for (int i = 0; i < 4; i++)
+	int chos;
+	cout << "choose input pattern or  show the example:  1:input pattern  |  2:example :";
+	cin >> chos;
+	double d[3];
+	if (chos == 1)
 	{
-		outpoint[i] = new vertex(out_loop[i]);
-		inpoint1[i] = new vertex(in_loop1[i]);
-		inpoint2[i] = new vertex(in_loop2[i]);
-		inpoint3[i] = new vertex(in_loop3[i]);
-		inpoint4[i] = new vertex(in_loop4[i]);
+		int in_loop_num;
+		cout << "input the number of inner loop:";
+		cin >> in_loop_num;
+		int num_vertex_out_lp;
+		cout << "input the number of vertex of out loop:";
+		cin >> num_vertex_out_lp;
+		std::vector<std::vector<double>>out_lp_vector;
+		std::vector<vertex*>out_point_ls;
+		std::vector<std::vector<vertex*>>in_point_ls;
+		for (int i = 0; i < num_vertex_out_lp; i++)
+		{
+			double xyz[3];
+			cout << "input the 3dcoord of " << i + 1 << " vertex of out loop: ";
+			cin >> xyz[0] >> xyz[1] >> xyz[2];
+			vertex* now_v = new vertex(xyz);
+			out_point_ls.push_back(now_v);
+		}
+		for (int i = 0; i < in_loop_num; i++)
+		{
+			int num_vertex_in_lp;
+			cout << "input the number of vertex of " << i + 1 << " inner loop : ";
+			cin >> num_vertex_in_lp;
+			std::vector<vertex*>now_in_point_ls;
+			for (int j = 0; j < num_vertex_in_lp; j++)
+			{
+				double xyz[3];
+				cout << "input the 3dcoord of " << j + 1 << " vertex of " << i + 1 << " inner loop : ";
+				cin >> xyz[0] >> xyz[1] >> xyz[2];
+				vertex* now_v = new vertex(xyz);
+				now_in_point_ls.push_back(now_v);
+			}
+			in_point_ls.push_back(now_in_point_ls);
+		}
+		cout << "input the sweeping vector: ";
+		cin >> d[0] >> d[1] >> d[2];
+
+		S = mvfs(out_point_ls[0]);
+		loop* out_lp = S->fc->lp;
+		for (int i = 0; i < out_point_ls.size() - 1; i++)
+		{
+			mev(out_point_ls[i], out_point_ls[i + 1], out_lp);
+		}
+		mef(out_point_ls[out_point_ls.size() - 1], out_point_ls[0], out_lp);
+
+		loop* now_lp;
+
+		for (int i = 0; i < in_point_ls.size(); i++)
+		{
+			now_lp = S->fc->next->lp;//增加内环开始
+			mev(out_point_ls[0], in_point_ls[i][0], now_lp);
+			for (int j = 0; j < in_point_ls[i].size() - 1; j++)
+			{
+				mev(in_point_ls[i][j], in_point_ls[i][j + 1], now_lp);
+			}
+			now_lp = kemr(out_point_ls[0], in_point_ls[i][0], now_lp);
+			loop* in_lp1 = mef(in_point_ls[i][in_point_ls[i].size() - 1], in_point_ls[i][0], now_lp);
+			kfmrh(out_lp, in_lp1);//增加内环结束
+		}
 	}
-	S = mvfs(outpoint[0]);
-	loop *out_lp = S->fc->lp;
-	for (int i = 0; i < 3; i++)
+	else
 	{
-		mev(outpoint[i], outpoint[i+1], out_lp);
+		double out_loop[][3] = { { 0.0, 0.0, 0.0 }, { 100.0, 0.0, 0.0 }, { 100.0, 100.0, 0.0 },{50.0,120.0,0.0}, { 0.0, 100.0, 0.0 } };
+		double in_loop1[][3]= { { 20.0, 10.0, 0.0 }, { 40.0, 10.0, 0.0 }, { 40.0, 30.0, 0.0 }, { 20.0, 30.0, 0.0 } };
+		double in_loop2[][3]= { { 30.0, 70.0, 0.0 }, { 50.0, 70.0, 0.0 }, { 50.0, 90.0, 0.0 }, { 30.0, 90.0, 0.0 } };
+		double in_loop3[][3] = { { 20.0, 40.0, 0.0 }, { 40.0, 40.0, 0.0 }, { 40.0, 60.0, 0.0 }, { 20.0, 60.0, 0.0 } };
+		double in_loop4[][3] = { { 70.0, 45.0, 0.0 }, { 90.0, 45.0, 0.0 }, { 90.0, 65.0, 0.0 }, { 70.0, 65.0, 0.0 } };
+		
+		d[0] = 10.0;
+		d[1] = 20.0;
+		d[2] = 30.0;
+		vertex *outpoint[5];
+		vertex *inpoint1[4];
+		vertex *inpoint2[4];
+		vertex *inpoint3[4];
+		vertex *inpoint4[4];
+		for (int i = 0; i < 4; i++)
+		{
+			outpoint[i] = new vertex(out_loop[i]);
+			inpoint1[i] = new vertex(in_loop1[i]);
+			inpoint2[i] = new vertex(in_loop2[i]);
+			inpoint3[i] = new vertex(in_loop3[i]);
+			inpoint4[i] = new vertex(in_loop4[i]);
+		}
+		outpoint[4] = new vertex(out_loop[4]);
+		S = mvfs(outpoint[0]);
+		loop *out_lp = S->fc->lp;
+		for (int i = 0; i < 4; i++)
+		{
+			mev(outpoint[i], outpoint[i+1], out_lp);
+		}
+		mef(outpoint[4], outpoint[0], out_lp);
+		loop* now_lp;
+
+		now_lp = S->fc->next->lp;//增加第一个内环开始
+		mev(outpoint[0], inpoint1[0], now_lp);
+		mev(inpoint1[0], inpoint1[1], now_lp);
+		mev(inpoint1[1], inpoint1[2], now_lp);
+		mev(inpoint1[2], inpoint1[3], now_lp);
+		now_lp = kemr(outpoint[0], inpoint1[0], now_lp);
+		loop *in_lp1 = mef(inpoint1[3], inpoint1[0], now_lp);
+		kfmrh(out_lp, in_lp1);//增加内环结束
+
+		now_lp = S->fc->next->lp;//增加第二个内环
+		mev(outpoint[0], inpoint2[0], now_lp);
+		mev(inpoint2[0], inpoint2[1], now_lp);
+		mev(inpoint2[1], inpoint2[2], now_lp);
+		mev(inpoint2[2], inpoint2[3], now_lp);
+		now_lp = kemr(outpoint[0], inpoint2[0], now_lp);
+		loop *in_lp2 = mef(inpoint2[3], inpoint2[0], now_lp);
+		kfmrh(out_lp, in_lp2);
+
+		now_lp = S->fc->next->lp;//增加第三个内环
+		mev(outpoint[0], inpoint3[0], now_lp);
+		mev(inpoint3[0], inpoint3[1], now_lp);
+		mev(inpoint3[1], inpoint3[2], now_lp);
+		mev(inpoint3[2], inpoint3[3], now_lp);
+		now_lp = kemr(outpoint[0], inpoint3[0], now_lp);
+		loop *in_lp3 = mef(inpoint3[3], inpoint3[0], now_lp);
+		kfmrh(out_lp, in_lp3);
+
+		now_lp = S->fc->next->lp;//增加第四个内环
+		mev(outpoint[0], inpoint4[0], now_lp);
+		mev(inpoint4[0], inpoint4[1], now_lp);
+		mev(inpoint4[1], inpoint4[2], now_lp);
+		mev(inpoint4[2], inpoint4[3], now_lp);
+		now_lp = kemr(outpoint[0], inpoint4[0], now_lp);
+		loop *in_lp4 = mef(inpoint4[3], inpoint4[0], now_lp);
+		kfmrh(out_lp, in_lp4);
 	}
-	mef(outpoint[3], outpoint[0], out_lp);
-
-	loop *now_lp = S->fc->next->lp;//增加第一个内环开始
-	mev(outpoint[0], inpoint1[0], now_lp);
-	mev(inpoint1[0], inpoint1[1], now_lp);
-	mev(inpoint1[1], inpoint1[2], now_lp);
-	mev(inpoint1[2], inpoint1[3], now_lp);
-	now_lp = kemr(outpoint[0], inpoint1[0], now_lp);
-	loop *in_lp1 = mef(inpoint1[3], inpoint1[0], now_lp);
-	kfmrh(out_lp, in_lp1);//增加内环结束
-
-	now_lp = S->fc->next->lp;//增加第二个内环
-	mev(outpoint[0], inpoint2[0], now_lp);
-	mev(inpoint2[0], inpoint2[1], now_lp);
-	mev(inpoint2[1], inpoint2[2], now_lp);
-	mev(inpoint2[2], inpoint2[3], now_lp);
-	now_lp = kemr(outpoint[0], inpoint2[0], now_lp);
-	loop *in_lp2 = mef(inpoint2[3], inpoint2[0], now_lp);
-	kfmrh(out_lp, in_lp2);
-
-	now_lp = S->fc->next->lp;//增加第三个内环
-	mev(outpoint[0], inpoint3[0], now_lp);
-	mev(inpoint3[0], inpoint3[1], now_lp);
-	mev(inpoint3[1], inpoint3[2], now_lp);
-	mev(inpoint3[2], inpoint3[3], now_lp);
-	now_lp = kemr(outpoint[0], inpoint3[0], now_lp);
-	loop *in_lp3 = mef(inpoint3[3], inpoint3[0], now_lp);
-	kfmrh(out_lp, in_lp3);
-
-	now_lp = S->fc->next->lp;//增加第四个内环
-	mev(outpoint[0], inpoint4[0], now_lp);
-	mev(inpoint4[0], inpoint4[1], now_lp);
-	mev(inpoint4[1], inpoint4[2], now_lp);
-	mev(inpoint4[2], inpoint4[3], now_lp);
-	now_lp = kemr(outpoint[0], inpoint4[0], now_lp);
-	loop *in_lp4 = mef(inpoint4[3], inpoint4[0], now_lp);
-	kfmrh(out_lp, in_lp4);
-	
 	sweep(S->fc, d);
 
 
